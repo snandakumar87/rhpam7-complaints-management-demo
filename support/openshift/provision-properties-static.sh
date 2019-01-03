24113626234f9052e4e3e5ec3d5f861c7c46331b
MyriamFentanes <myriam.fentanes@gmail.coms>
2019-01-03 14:44:26 -0500

    First Commit

---
+++ support/openshift/provision-properties-static.sh
@@ -0,0 +1,16 @@
+PRJ_NAME="rhpam7-oih"
+PRJ_DESCRIPTION="Red Hat Process Automation Manager 7 Order IT Hardware Demo"
+
+#GitHub tag referencing the image streams and templates.
+OPENSHIFT_PAM7_TEMPLATES_TAG=7.0.2.GA
+
+BUSINESS_CENTRAL_REST_URL="$BC_URL/rest"
+
+SPACE="MySpace"
+DEFAULT_GROUPID="com.myspace"
+#PROJECT_GIT="https://github.com/DuncanDoyle/IT_Orders.git"
+PROJECT_GIT="https://github.com/snandakumar87/ComplaintsManagementWorkflow"
+PROJECT_ID="ComplaintsManagement"
+PROJECT_NAME="ComplaintsManagement"
+USERNAME="adminUser"
+PASSWORD="test1234!"
