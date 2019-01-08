932c82c0b076e21c13b9c33042a1ef3fc1759232
MyriamFentanes <myriam.fentanes@gmail.coms>
2019-01-08 11:51:02 -0500

    Revising property files

--- support/openshift/provision-properties-static.ps1
+++ support/openshift/provision-properties-static.ps1
@@ -1,5 +1,5 @@
-$PRJ_NAME="rhpam7-oih"
-$PRJ_DESCRIPTION="Red Hat Process Automation Manager 7 Order IT Hardware Demo"
+$PRJ_NAME="rhpam7-complaints-management"
+$PRJ_DESCRIPTION="Red Hat Process Automation Manager 7 Complaints Management"
 
 #GitHub tag referencing the image streams and templates.
 $OPENSHIFT_PAM7_TEMPLATES_TAG="7.0.2.GA"
